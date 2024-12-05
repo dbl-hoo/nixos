dc5e38ccdbd6218ee787c4de6d47ec4ac40d7093
Jason Kirkham <jason.kirkham@gmail.com>
2024-12-05 12:59:08 -0500

    reverted flake

--- flake.nix
+++ flake.nix
@@ -2,22 +2,26 @@
   description = "Kirkham Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }: {
-    homeConfigurations = {
-      kirkham = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Use the provided nixpkgs input
-        modules = [ ./home.nix ]; # Ensure `home.nix` exists and is valid
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
       };
     };
 
-    # Optional defaultPackage definition for `nix run .`
-    defaultPackage.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.hello; # Example
+    homeConfigurations = {
+      kirkham = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [ ./home.nix ];
+      };
+    };
   };
 }
